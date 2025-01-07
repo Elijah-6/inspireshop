2e937dc1023474f562808bd7b6b3bc2faf0d33e4
Elijah Frempong <elijahfrimpong76@gmail.com>
2025-01-07 20:23:12 +0000

    side navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import {StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import {Route, RouterProvider, createRoutesFromElements} from 'react-router';
