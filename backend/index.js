2aab06570881bd1b8cdd96ff7cdb3f8d75214d67
Elijah Frempong <elijahfrimpong76@gmail.com>
2025-01-08 12:43:49 +0000

    server update

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import userRoutes from './routes/userRoutes.js';
 
 // utils
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 
 // Load environment variables from .env file
@@ -22,7 +26,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
-app.use("/api/v1/users", userRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
+
+// app.get("/api/config/paypal", (req, res) => {
+//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+// });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+// listen on port 5000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
